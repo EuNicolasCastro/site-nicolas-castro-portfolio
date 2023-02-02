9e19ecc5bc896eb73dc474c9e92e5f24c41709c2
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-02-02 09:57:08 -0300

    colocando transicao nos botoes do menu principal

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -11,11 +11,15 @@ secaoSobre.classList.add('desaparecer');
 function aparecerSecaoPortfolio(){
   secaoPortfolio.classList.remove('desaparecer');
   secaoSobre.classList.add('desaparecer');
+  btnportfolio.style.fontWeight='600';
+  btnsobre.style.fontWeight = '400';
   secaoPortfolio.innerHTML = ('<iframe src="portfolio.html" id="portfolio" heigh"100%"  ></iframe>');
 }
 
 function aparecerSecaoSobre(){
   secaoSobre.classList.remove('desaparecer');
   secaoPortfolio.classList.add('desaparecer');
+  btnportfolio.style.fontWeight='400';
+  btnsobre.style.fontWeight = '600';
   secaoSobre.innerHTML = ('<iframe src="sobre.html" id="sobre-mim" ></iframe>');
 }
